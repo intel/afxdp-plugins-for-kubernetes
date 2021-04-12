4319fefe1f7d6c812b40347a8a2614e6f87d429c
root <root@silpixa00397259.ir.intel.com>
2021-04-12 18:01:00 +0000

    Update CNDP UDS
    Update the code to properly implement the agreed UDS protocol.
    Added code to discover the pod (host)name via the resource api.
    Updated e2e test to reflect changes.

--- examples/e2e-test/README.md
+++ examples/e2e-test/README.md
@@ -5,55 +5,191 @@ Bash script to run an e2e test of deceive plugin and CNI.
 ## Assumptions 
 - Kubernetes installed and running
 - Multus installed
+- Availability of at least 2 AF_XDP capable netdevs
 ## Run Test
 - Run `build.sh` in the root directory of the repo.
-- cd to this directory, script must be run from here.
-- Place a `config.json` file here that includes the name of actual netdev(s) that you would like to attach to the test pod.
+- Navigate to this directory (`examples/e2e-test/`). Script must be run from here.
+- Place a `config.json` file here that includes the name of the netdevs that you would like to make available to the test pod.
 	- A sample config can be found in `examples/sample-config/`
-	- For this test there must be a pool named `e2e` with real netdev(s).
+	- For this test there must be a pool named `e2e` with at least 2 real netdevs.
 - Run the test script `./e2e-test.sh`
 ## What Happens
 - The CNI binary is moved to /opt/cni/bin. The binary is uniquely named for this test so it will not impact an existing install.
 - A network attachment definition is created. The NAD is uniquely named for this test so it will not impact an existing NAD.
-- A sample app is built.
-	- Currently an echo server to test the UDS.
-	- This will be updated to xdpsock and later CNDP.
-- Sample app is built into an Docker image. The image is uniquely tagged for this test so it will not overwrite existing images.
-- The Device Plugin is started, devices in `config.json` are advertised to Kubernetes.
+- A sample app is built. This sample app tests the handshake over the UDS.
+- Sample app is built into a Docker image. The image is uniquely tagged so it will not overwrite existing images.
+- The device plugin is started and devices listed in `config.json` are advertised to Kubernetes.
 - A pod is created, a single container running the Docker image. The pod is uniquely named for this test.
 - `ip a` is run in the pod to show attached interfaces.
-- The sample app in the pod is run:
-	- Currently the sample app writes a message to the DP over the UDS.
-	- If all works correctly the DP will echo back the message.
-	- Later we'll test xdpsock and CNDP here.
-- Everything above is cleaned up at the beginning and end of each run.
+- The sample app in the pod is run. The app makes requests via the UDS.
+	- Cycles through and tests the full UDS handshake protocol.
+	- Some bad requests are sent to generate expected errors.
+	- All requests and responses are printed to screen.
+- Everything above is cleaned up at the end of each run.
 ## Test Output
-- A successful test will show three netdevs in the pod.
+A successful test will show three netdevs in the pod:
+- lo - the loopback interface
+- eth0 - the default network interface
+- The CNDP interface, in this example named ens785f0
+ 
 ```
-*****************************************************
-*              Netdevs attached to pod              *
-*****************************************************
+***** Netdevs attached to pod *****
+
 1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
     link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
     inet 127.0.0.1/8 scope host lo
        valid_lft forever preferred_lft forever
-3: eth0@if303: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1450 qdisc noqueue state UP group default
-    link/ether a2:3c:12:e2:06:a7 brd ff:ff:ff:ff:ff:ff link-netnsid 0
-    inet 10.244.0.30/24 scope global eth0
+3: eth0@if430: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1450 qdisc noqueue state UP group default
+    link/ether a6:76:2a:39:48:98 brd ff:ff:ff:ff:ff:ff link-netnsid 0
+    inet 10.244.0.146/24 brd 10.244.0.255 scope global eth0
        valid_lft forever preferred_lft forever
-11: ens785f0: <BROADCAST,MULTICAST,PROMISC> mtu 1500 qdisc noop state DOWN group default qlen 1000
-    link/ether 98:03:9b:6a:b4:ee brd ff:ff:ff:ff:ff:ff
+13: ens785f0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 xdp/id:1234 qdisc mq state DOWN group default qlen 1000
+    link/ether 98:03:9b:6a:b4:ef brd ff:ff:ff:ff:ff:ff
+    inet 192.168.1.202/24 brd 192.168.1.255 scope global ens785f0
+       valid_lft forever preferred_lft forever
 
 ```
-- A successful test will also show the sample app worked correctly, verifying DP functionality.
-	- Currently success means the DP echoes back a message sent from the sample app. This will evolve.
+A successful test will also show the output from the test app as it runs the handshake protocol over the UDS. Some responses containing errors are expected here as the app intentionally sends bad requests.
+
+It should also be noted that the test app will request the file descriptor for every device known to the device plugin. Every request will result in an error response, except for requests associated with devices that are actually assigned to the current pod.
 ```
-*****************************************************
-*                     UDS Test                      *
-*****************************************************
-I1112 11:33:50.963770   86982 cndp.go:49] Client connected
-I1112 11:33:50.963859   86982 cndp.go:58] Received: end to end test
-I1112 11:33:50.964076   86982 cndp.go:58] Received: exit
-I1112 11:33:50.964105   86982 cndp.go:71] Closing connection
--> Received: Hello from DP, you said: end to end test
+***** UDS Test *****
+
+I0422 13:13:48.988962  317356 cndp.go:141] Client connected
+I0422 13:13:48.989044  317356 cndp.go:153] Request: /connect
+I0422 13:13:48.989062  317356 cndp.go:199] Response: /host
+
+Request: /connect
+Response: /host
+
+I0422 13:13:50.989440  317356 cndp.go:153] Request: "hostname": "cndp-e2e-test"
+I0422 13:13:50.989470  317356 cndp.go:217] Validating pod hostname: cndp-e2e-test
+I0422 13:13:50.990405  317356 cndp.go:231] cndp-e2e-test found on node
+I0422 13:13:50.990437  317356 cndp.go:261] cndp-e2e-test is valid for this UDS connection
+I0422 13:13:50.990451  317356 cndp.go:199] Response: /host_ok
+
+Request: "hostname": "cndp-e2e-test"
+Response: /host_ok
+
+I0422 13:13:52.990889  317356 cndp.go:153] Request: "hostname": "bad-hostname"
+I0422 13:13:52.990916  317356 cndp.go:217] Validating pod hostname: bad-hostname
+I0422 13:13:52.991603  317356 cndp.go:233] bad-hostname not found on node
+I0422 13:13:52.991632  317356 cndp.go:199] Response: Error: Invalid Host
+
+Request: "hostname": "bad-hostname"
+Response: Error: Invalid Host
+
+I0422 13:13:54.992114  317356 cndp.go:153] Request: /xsk_map_ens785f0
+I0422 13:13:54.992156  317356 cndp.go:199] Response: 6
+
+Request: /xsk_map_ens785f0
+Response: 6
+
+I0422 13:13:56.992557  317356 cndp.go:153] Request: /xsk_map_ens785f1
+I0422 13:13:56.992600  317356 cndp.go:199] Response: Error: Unknown Interface ens785f1
+
+Request: /xsk_map_ens785f1
+Response: Error: Unknown Interface ens785f1
+
+I0422 13:13:58.993017  317356 cndp.go:153] Request: /fin
+I0422 13:13:58.993056  317356 cndp.go:199] Response: /fin_ack
+I0422 13:13:58.993081  317356 cndp.go:211] Closing connection
+
+Request: /fin
+Response: /fin_ack
+
+```
+
+## Extended Test
+The e2e test script can also do an extended run. In addition to the single container single device test, the script will go on to create:
+- A pod with a single container requesting 2 devices
+- A pod with 2 containers, each requesting a single device
+
+To do the full extended run, add the flag -f or --full when calling the script:
+`./e2e-test.sh --full`
+
+## Manual  Test
+It can be useful to run the test manually. For instance, allowing the user time to log into the pod, check the environment and debug. Start the following steps from the root directory of the repo.
+```
+# build the plugins
+./build.sh
+
+# move to the e2e test dir
+cd examples/e2e-test/
+
+# install the cni plugin
+cp ../../bin/cndp /opt/cni/bin/cndp-e2e
+
+# create the NAD
+kubectl create -f ./nad.yaml
+
+# build the sample apps
+go build -o uds-client-auto ./autoTest/main.go
+go build -o uds-client-manual ./manualTest/main.go
+
+# build the docker image (these 8 lines are a single command)
+docker build \
+--build-arg http_proxy=${http_proxy} \
+--build-arg HTTP_PROXY=${HTTP_PROXY} \
+--build-arg https_proxy=${https_proxy} \
+--build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+--build-arg no_proxy=${no_proxy} \
+--build-arg NO_PROXY=${NO_PROXY} \
+-t cndp-e2e-test -f Dockerfile .
+
+# start the device plugin
+./../../bin/cndp-dp
+```
+At this point the device plugin will consume your terminal. Open a new terminal and navigate to this directory (`examples/e2e-test/`) to continue.
+
+```
+# create the pod
+kubectl create -f pod-1c1d.yaml
+
+# open a terminal into the pod
+kubectl exec -it cndp-e2e-test -- bash
+
+# sample apps can be found in /cndp
+cd /cndp/
+```
+There are 2 sample apps in `/cndp`:
+ - uds-client-auto
+ - uds-client-manual
+
+The auto app will run through the full UDS handshake protocol. This is the app used in the automated e2e tests.
+The manual app gives you a prompt and allows you to manually write messages to the UDS. Type `exit` to exit out of this app.
+
+**Note:** The device plugin is designed to give a single session over the UDS. After this it closes the connection and stops listening. This means the test apps can only be run once in a pods lifecycle. Attempting to run the app a second time will fail.
+
+Example usage of the manual app:
+```
+root@cndp-e2e-test:/cndp# ./uds-client-manual
+
+-> /connect
+Received: /host
+
+-> "hostname": "cndp-e2e-test"
+Received: /host_ok
+
+-> "hostname": "bad-host"
+Received: Error: Invalid Host
+
+-> /xsk_map_ens785f0
+Received: 6
+
+-> /xsk_map_bad-device
+Received: Error: Unknown Interface bad-device
+
+-> foobar
+Received: Error: Bad Request
+
+-> /foobar
+Received: Error: Bad Request
+
+-> /fin
+Received: /fin_ack
+
+-> exit
+
+root@cndp-e2e-test:/cndp#
 ```
--- pkg/cndp/cndp.go
+++ pkg/cndp/cndp.go
@@ -18,8 +18,43 @@ package cndp
 import (
 	"github.com/golang/glog"
 	"github.com/nu7hatch/gouuid"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	podresourcesapi "k8s.io/kubelet/pkg/apis/podresources/v1alpha1"
 	"net"
+	"os"
 	"strings"
+	"time"
+)
+
+/*CNDP UDS*/
+const (
+	requestConnect    = "/connect"
+	requestHostname   = "/host"
+	requestXskMap     = "/xsk_map_"
+	requestXskSock    = "/xsk_sock_"
+	requestFdReceived = "/fd_recvd"
+	requestFin        = "/fin"
+
+	responseFinAck = "/fin_ack"
+	responseHostKv = "\"hostname\":"
+	responseHostOk = "/host_ok"
+
+	errorBadRequest     = "Error: Bad Request"
+	errorNotImplemented = "Error: Not Implemented"
+	errorBadHost        = "Error: Invalid Host"
+	errorHostError      = "Error: Error occurred during host validation"
+
+	udsBufSize     = 1024
+	usdSockDir     = "/tmp/"
+	udsIdleTimeout = 60 * time.Second
+)
+
+/*Pod Resources API*/
+const (
+	podResSockDir  = "/var/lib/kubelet/pod-resources"
+	podResSockPath = podResSockDir + "/kubelet.sock"
+	podResTimeout  = 10 * time.Second
 )
 
 /*
@@ -27,8 +62,8 @@ Interface is the interface to the cndp package, representing CNDP to the rest of
 All interactions with CNDP should be done with an object implementing this interface.
 */
 type Interface interface {
-	StartSocketServer(SockAddr string)
-	CreateUdsSocket() string
+	StartUdsServer(server UdsServer)
+	CreateUdsSocketPath() string
 }
 
 /*
@@ -40,73 +75,274 @@ type CNDP struct {
 }
 
 /*
-StartSocketServer starts listening on the UDS socket for calls from CNDP.
-*/
-func (c *CNDP) StartSocketServer(SockAddr string) {
-
-	//TODO currently rough sample code, update this to provide the FD to xdpsock, proper error and socket handeling
-	//TODO later update with protocol for interacting with cndp
-	//TODO the go routine should be in here, not out in poolManager
-
-	glog.Info("Listening on socket " + SockAddr)
-
-	l, err := net.Listen("unix", SockAddr)
-	if err != nil {
-		glog.Fatal("listen error:", err)
-	}
-
-	// Accept new connections
-	conn, err := l.Accept()
-	if err != nil {
-		glog.Fatal("accept error:", err)
-	}
-
-	glog.Info("Client connected")
-
-	buf := make([]byte, 1024)
-	for {
-		n, err := conn.Read(buf[:])
-		if err != nil {
-			glog.Fatal("read error:", err)
-		}
-
-		glog.Info("Received: " + string(buf[0:n]))
-
-		if strings.Compare("exit", string(buf[0:n])) == 0 {
-			break
-		}
-
-		_, err = conn.Write([]byte("Hello from DP, you said: " + string(buf[0:n])))
-		if err != nil {
-			glog.Fatal("write error:", err)
-		}
-
-	}
-
-	glog.Info("Closing connection")
-	conn.Close()
-	l.Close()
-}
-
-/*
-CreateUdsSocket creates a Unix Domain Socket that will be mounted into the pod.
-This UDS is used for interacting with CNDP.
-*/
-func (c *CNDP) CreateUdsSocket() string {
-
-	sockName, err := uuid.NewV4() //TODO check if it exists
-
-	if err != nil {
-		glog.Fatal(err)
-	}
-
-	return "/tmp/" + sockName.String() + ".sock"
-}
-
-/*
 NewCndp returns a CNPD object of type cndp.Interface.
 */
 func NewCndp() Interface {
-	//TODO also return error?
 	return &CNDP{}
+}
+
+/*
+UdsServer is the struct representing the UDS Server.
+The server runs on a Go routine and serves XDP info to the pod.
+*/
+type UdsServer struct {
+	Socket     string
+	DeviceType string
+	Devices    map[string]string
+}
+
+/*
+CreateUdsSocketPath generates a unique filename/filepath for the Unix Domain Socket (UDS).
+This UDS will be mounted into the pod and is used for communicating with CNDP.
+*/
+func (c *CNDP) CreateUdsSocketPath() string {
+	var sockPath string
+
+	for {
+		sockName, err := uuid.NewV4()
+		if err != nil {
+			glog.Error(err)
+		}
+
+		sockPath = usdSockDir + sockName.String() + ".sock"
+		if _, err := os.Stat(sockPath); os.IsNotExist(err) {
+			break
+		}
+		glog.Info(sockPath + " already exists. Regenerating.")
+	}
+
+	return sockPath
+}
+
+/*
+StartUdsServer is the public facing function for starting a UDS server
+It runs a private startUdsServer function on a Go Routine
+*/
+func (c *CNDP) StartUdsServer(server UdsServer) {
+	go startUdsServer(server)
+}
+
+/*
+startUdsServer is the a private function, run on a Go Routine from the public StartUdsServer.
+This is the main loop of the UDS server. It listens for and serves only a single connection.
+Across this single connection we validate host and serve file descriptors to the CNDP app.
+*/
+func startUdsServer(server UdsServer) {
+	glog.Info("Initialising UDS server on socket " + server.Socket)
+
+	//create UDS
+	addr, err := net.ResolveUnixAddr("unix", server.Socket)
+	if err != nil {
+		glog.Error("Error resolving Unix address "+server.Socket+": ", err)
+		return
+	}
+
+	//create UDS listener
+	listener, err := net.ListenUnix("unix", addr)
+	if err != nil {
+		glog.Error("Error creating Unix listener for "+server.Socket+": ", err)
+		return
+	}
+	defer func() {
+		glog.Info("Closing Unix listener")
+		listener.Close()
+	}()
+
+	//set UDS listener timeout
+	err = listener.SetDeadline(time.Now().Add(udsIdleTimeout))
+	if err != nil {
+		glog.Error("Error setting listener timeout: ", err)
+		return
+	}
+
+	glog.Info("UDS server initialised. Listening for new connection.")
+
+	//listen for new connection
+	conn, err := listener.Accept()
+	if err != nil {
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			glog.Error("Listener timed out: ", err)
+			return
+		}
+		glog.Error("Listener Accept error: ", err)
+		return
+	}
+	defer func() {
+		glog.Info("Closing connection")
+		conn.Close()
+	}()
+
+	glog.Info("New connection. Waiting for requests.")
+	connected := true
+	buf := make([]byte, udsBufSize)
+
+	for connected {
+		//(re)set connection timeout
+		err = conn.SetDeadline(time.Now().Add(udsIdleTimeout))
+		if err != nil {
+			glog.Error("Error setting connection timeout: ", err)
+			return
+		}
+
+		//read incomming requests
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				glog.Error("Connection timed out: ", err)
+				return
+			}
+			glog.Error("Connection read error: ", err)
+		}
+
+		request := string(buf[0:n])
+		glog.Info("Request: " + request)
+
+		var response string
+
+		//handle request
+		switch {
+		case request == requestConnect:
+			response = requestHostname
+
+		case strings.Contains(request, responseHostKv):
+			response = server.handleHostValidation(request)
+
+		case strings.Contains(request, requestXskMap):
+			response = server.handleXskRequest(request)
+
+		case strings.Contains(request, requestXskSock):
+			response = errorNotImplemented
+
+		case request == requestFdReceived:
+			response = ""
+
+		case request == requestFin:
+			response = responseFinAck
+			connected = false
+
+		default:
+			response = errorBadRequest
+		}
+
+		//send response
+		if response != "" {
+			glog.Info("Response: " + response)
+			_, err = conn.Write([]byte(response))
+			if err != nil {
+				glog.Error("Connection write error: ", err)
+				return
+			}
+		}
+	}
+}
+
+func (s *UdsServer) handleHostValidation(request string) string {
+	hostname := strings.Trim(strings.TrimPrefix(request, responseHostKv), " \"")
+	valid, err := s.validateHost(hostname)
+	if err != nil {
+		glog.Error("Error validating host "+hostname+": ", err)
+		return errorHostError
+	}
+
+	if valid {
+		return responseHostOk
+	}
+
+	return errorBadHost
+}
+
+func (s *UdsServer) handleXskRequest(request string) string {
+	iface := strings.ReplaceAll(request, requestXskMap, "")
+	if descriptor, ok := s.Devices[iface]; ok {
+		return descriptor
+	}
+
+	return "Error: Interface not valid for this pod: " + iface
+}
+
+func (s *UdsServer) validateHost(hostname string) (bool, error) {
+	glog.Info("Validating pod hostname: " + hostname)
+
+	resp, err := getPodResources(podResSockPath)
+	if err != nil {
+		glog.Error("Error Getting pod resources: ", err)
+		return false, err
+	}
+
+	podResourceMap := make(map[string]podresourcesapi.PodResources)
+
+	for _, pod := range resp.GetPodResources() {
+		podResourceMap[pod.GetName()] = *pod
+	}
+
+	if _, ok := podResourceMap[hostname]; ok {
+		glog.Info(hostname + " found on node")
+	} else {
+		glog.Info(hostname + " not found on node")
+		return false, nil
+	}
+
+	pod := podResourceMap[hostname]
+	valid := false
+
+	for _, container := range pod.GetContainers() {
+		for _, device := range container.GetDevices() {
+
+			if device.GetResourceName() != s.DeviceType ||
+				len(device.GetDeviceIds()) != len(s.Devices) {
+				//not the resource we're interested in
+				//or this container has a different number of the resource
+				continue
+			}
+
+			//compare known devices (from Allocate) vs devices from resource api
+			for _, dev := range device.GetDeviceIds() {
+				if _, exists := s.Devices[dev]; exists {
+					valid = true //valid while devices match
+				} else {
+					valid = false
+					continue //not valid if any device does not match
+				}
+			}
+
+			if valid {
+				glog.Info(hostname + " is valid for this UDS connection")
+				return true, nil
+			}
+		}
+	}
+
+	glog.Info(hostname + " could not be validated for this UDS connection")
+	return false, nil
+}
+
+func getPodResources(socket string) (*podresourcesapi.ListPodResourcesResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), podResTimeout)
+	defer cancel()
+
+	glog.Info("Opening Pod Resource API connection")
+	conn, err := grpc.DialContext(ctx, socket, grpc.WithInsecure(), grpc.WithBlock(),
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}),
+	)
+	if err != nil {
+		glog.Error("Error connecting to Pod Resource API: ", err)
+		return nil, err
+	}
+	defer func() {
+		glog.Info("Closing Pod Resource API connection")
+		conn.Close()
+	}()
+
+	glog.Info("Requesting pod resource list")
+	client := podresourcesapi.NewPodResourcesListerClient(conn)
+
+	resp, err := client.List(ctx, &podresourcesapi.ListPodResourcesRequest{})
+	if err != nil {
+		glog.Error("Error getting Pod Resource list: ", err)
+		return nil, err
+	}
+
+	return resp, nil
 }
