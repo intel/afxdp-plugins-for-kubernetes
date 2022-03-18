4d4590200accf9ebf873044e0edf1e417eac16ee
Gary Loughnane <gary.loughnane@intel.com>
2022-03-18 12:21:53 +0000

    Rename vars in entrypoint (#76)

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-CNDP_DIR="/afxdp"
+BINS_DIR="/afxdp"
 DP_BIN="afxdp-dp"
-DP_CONFIG_FILE=$CNDP_DIR/"config/config.json"
+DP_CONFIG_FILE=$BINS_DIR/"config/config.json"
 CNI_BIN="afxdp"
 CNI_BIN_DIR="/opt/cni/bin"
 
-cp -f $CNDP_DIR/$CNI_BIN $CNI_BIN_DIR/$CNI_BIN
-exec $CNDP_DIR/$DP_BIN -config $DP_CONFIG_FILE
+cp -f $BINS_DIR/$CNI_BIN $CNI_BIN_DIR/$CNI_BIN
+exec $BINS_DIR/$DP_BIN -config $DP_CONFIG_FILE
