aa1f7ae0adf7b38cc849339b2a7e1a8a52ea993a
Gary Loughnane <gary.loughnane@intel.com>
2022-09-01 15:44:38 +0100

    Move KernelInt test to tools package (#42)
    Function was moved previously without the test

--- cmd/deviceplugin/main_test.go
+++ cmd/deviceplugin/main_test.go
@@ -18,7 +18,6 @@ package main
 import (
 	"github.com/intel/afxdp-plugins-for-kubernetes/internal/deviceplugin"
 	"github.com/intel/afxdp-plugins-for-kubernetes/internal/host"
-	"github.com/intel/afxdp-plugins-for-kubernetes/internal/tools"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -128,65 +127,4 @@ func TestCheckHost(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestIntVersion(t *testing.T) {
-	testCases := []struct {
-		name      string
-		version   string
-		expResult int64
-		expError  error
-	}{
-		{
-			name:      "first_test_5.4.0-89-generic",
-			version:   "5.4.0-89-generic",
-			expResult: 500040000,
-			expError:  nil,
-		},
-		{
-			name:      "second_test_5.4.0-89",
-			version:   "5.4.0-89",
-			expResult: 500040000,
-			expError:  nil,
-		},
-		{
-			name:      "third_test_5.4.0-generic",
-			version:   "5.4.0-generic",
-			expResult: 500040000,
-			expError:  nil,
-		},
-		{
-			name:      "fourth_test_5.4.0",
-			version:   "5.4.0",
-			expResult: 500040000,
-			expError:  nil,
-		},
-		{
-			name:      "fifth_test_5.4.0--generic",
-			version:   "5.4.0--generic",
-			expResult: 500040000,
-			expError:  nil,
-		},
-		{
-			name:      "sixth_test_5.4.0-43-.generic",
-			version:   "5.4.0-43-.generic",
-			expResult: 500040000,
-			expError:  nil,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-
-			actualReturn, err := tools.KernelVersionInt(tc.version)
-
-			assert.Equal(t, tc.expResult, actualReturn, "Returned value does not match expected value")
-
-			if err != nil {
-				require.Error(t, tc.expError, err, "Error was expected")
-				assert.Contains(t, err.Error(), tc.expError.Error(), "Unexpected error returned")
-			}
-		})
-	}
-
 }
